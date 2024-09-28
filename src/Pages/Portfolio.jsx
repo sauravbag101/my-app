fdc20a4191b35a326cdb982b949006ec745c5458
sauravbag101 <sauravbag101@gmail.com>
2024-09-28 20:30:22 +0530

    qqq

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -63,7 +63,7 @@ const Portfolio = () => {
           </h1>
           <p className="title">Web Full-Stack Developer</p>
         </div>
-        <a target="_blank" href="https://drive.google.com/file/d/1TT-nZV9m_moSWoUBHm4CVU5Qa2tNfUU5/view?usp=sharing"  rel="noreferrer">
+        <a target="_blank" href="https://drive.google.com/file/d/1nVNFW9V63upl_YZuHbcZ4-hN3xq-SLDk/view?usp=sharing"  rel="noreferrer">
         <button type="button" className=" flex align-center py-2 px-3  text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
         border-gray-200 hover:bg-gray-100  focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 
         dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
